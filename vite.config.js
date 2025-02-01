d9b628e127a385bbe4ed2c3dc1c3a82d70ad44c7
cramination <camnation@comcast.net>
2025-02-01 11:57:02 -0500

    gltf vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import gltf from 'vite-plugin-gltf';
  
  export default defineConfig({
+    plugins: [gltf()],
     assetsInclude: ['src/models/test-cube.gltf'],
     base: "/comp-3/",
     server: {
