0e80890378153f983f8e58f5aac20bbe9f6edb22
cramination <camnation@comcast.net>
2025-01-31 19:58:55 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+ 
+ export default defineConfig({
+    base: "/comp-3/",
+    server: {
+
+        port: 5137 // Change this to your desired port
+
+    }
+
+}
+)
